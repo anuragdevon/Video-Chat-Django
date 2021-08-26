6b5b38c516842ba8bc8d28fd8d323411723b48bb
anuragdevon <msm19b021@iiitdm.ac.in>
2021-08-26 15:00:28 +0530

    add basic environ settings, connect mongodb, import tokens

---
+++ agora/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# from . import views
+
+# routing
+urlpatterns = [
+    path('', ),
+    path('', ),
+    path('', ),
+    path('', )
+]
+# https://github.com/AgoraIO/Basic-Video-Broadcasting
