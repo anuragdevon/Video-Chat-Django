9bb9f3f84784fdc29cb652c6fc3b6d7eb2b459d8
anuragdevon <msm19b021@iiitdm.ac.in>
2021-09-06 20:00:17 +0530

    setup environ variables, splitup django.settings

---
+++ VideoChat/settings/developement.py
@@ -0,0 +1,51 @@
+# imports
+try:
+    import os
+    from base import *
+
+except Exception as e:
+    print(e)
+
+# DEBUG MODE
+DEBUG = os.environ['DEBUG']
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS += [
+
+    # developement specific
+    'debug_toolbar'
+]
+
+# Debug Toolbar settings
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+SHOW_TOOLBAR = os.environ['SHOW_TOOLBAR']
+def show_toolbar(request):
+    return SHOW_TOOLBAR
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar
+}
+
+# Local Mongo DB(NOSQL)
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'agora_database',
+    }
+}
---
+++ VideoChat/settings/production.py
@@ -0,0 +1,34 @@
+try:
+    import os
+    from base import *
+
+except Exception as e:
+    print(e)
+
+# DEBUG MODE
+DEBUG = os.environ['DEBUG']
+
+ALLOWED_HOSTS = ['ip-address', 'https://zyz.com/']
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'agora_database',
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': ''
+    }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.psycopg2',
+#         'NAME': os.environ['DB_NAME'],
+#         'USER': os.environ['DB_USER'],
+#         'PASSWORD': os.environ['DB_PASSWORD'],
+#         'HOST': os.environ['DB_HOST'],
+#         'PORT': ''
+#     }
+# }
